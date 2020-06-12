5a21ea7d4397532d9e2aae15980a92208b09f9b9
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-06-12 15:35:22 +0900

    Todo絞り込み修正

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -13,9 +13,11 @@ class UserController extends Controller
         $user  = User::where('nickname', $nickname)->firstOrFail();
         $query = Todo::where('user_id', $user->id);
         if ($request->input('incomplete')) {
-            $query->where('status', '0');
+            $query->where('status', '0')
+                    ->orderBy('due_date', 'asc')
+                    ->get();
         }
-        $todos = $query->orderByDesc('id')->get();
+        $todos = $query->orderBy('id', 'desc')->get();
         return view('user.show', compact('user', 'todos'));
     }
 }
